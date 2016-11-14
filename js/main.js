d16a9b7c692afe8b1c8bc5b886bd88273b1da5a3
Alec Lomas <alec@lowmess.com>
2016-11-13 21:23:17 -0700

    Updates hero patterns

--- js/main.js
+++ js/main.js
@@ -12,38 +12,32 @@ let fill = '333333'
 let opacity = 0.7
 
 let backgrounds = [
+  hero.fancyRectangles(fill, opacity),
+  hero.overlappingCircles(fill, opacity),
+  hero.plus(fill, opacity),
+  hero.volcanoLamp(fill, opacity),
+  hero.wiggle(fill, opacity),
   hero.bubbles(fill, opacity),
   hero.cage(fill, opacity),
-  hero.connections(fill, opacity),
   hero.current(fill, opacity),
   hero.diagonalStripes(fill, opacity),
   hero.flippedDiamonds(fill, opacity),
   hero.houndstooth(fill, opacity),
   hero.linesInMotion(fill, opacity),
-  hero.moroccan(fill, opacity),
   hero.morphingDiamonds(fill, opacity),
-  hero.rails(fill, opacity),
   hero.rain(fill, opacity),
-  hero.squaresInSquares(fill, opacity),
   hero.stripes(fill, opacity),
   hero.ticTacToe(fill, opacity),
   hero.zigZag(fill, opacity),
   hero.bankNote(fill, opacity),
   hero.boxes(fill, opacity),
-  hero.circlesAndSquares(fill, opacity),
   hero.circuitBoard(fill, opacity),
   hero.diagonalLines(fill, opacity),
-  hero.endlessClouds(fill, opacity),
-  hero.eyes(fill, opacity),
   hero.floorTile(fill, opacity),
-  hero.intersectingCircles(fill, opacity),
   hero.melt(fill, opacity),
   hero.overlappingDiamonds(fill, opacity),
   hero.parkayFloor(fill, opacity),
-  hero.polkaDots(fill, opacity),
-  hero.signal(fill, opacity),
-  hero.slantedStars(fill, opacity),
-  hero.wallpaper(fill, opacity)
+  hero.polkaDots(fill, opacity)
 ]
 
 hero.patterns.forEach(function (item, index, array) {
