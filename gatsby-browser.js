121457b13951e68619d284a739f5da8481408915
Alec Lomas <alec@lowmess.com>
2018-01-09 18:39:25 -0700

    :wrench: point to updated stats server
    should probably change this to an env variable but that sounds annoying too

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,7 +7,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://stats-kukzlxcbiu.now.sh/graphql',
+    uri: 'https://stats-pzrqiaginh.now.sh/graphql',
     method: 'POST',
   }),
   cache: new InMemoryCache(),
