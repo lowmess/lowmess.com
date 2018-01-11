1694b94561ec28883645e2739ab36d20f3f10c99
Alec Lomas <alec@lowmess.com>
2018-01-11 04:04:01 -0700

    :fire: remove unnecessary display property

--- src/components/Stat.js
+++ src/components/Stat.js
@@ -3,16 +3,12 @@ import { Box, Flex, Text } from './Layout'
 
 const Stat = ({ title, children, ...props }) => (
   <Flex align="baseline" flexDirection={['column', 'row']} {...props}>
-    <Box
-      width={[1, 1 / 3, 1 / 4]}
-      display={['block', 'inline-block']}
-      mb={[1, 0]}
-    >
+    <Box width={[1, 1 / 3, 1 / 4]} mb={[1, 0]}>
       <Text fontSize={[2, 3]} fontWeight={5} lineHeight="copy">
         {title}
       </Text>
     </Box>
-    <Box width={[1, 2 / 3, 3 / 4]} display={['block', 'inline-block']}>
+    <Box width={[1, 2 / 3, 3 / 4]}>
       <Text fontFamily="monospace" fontSize={[1, 2]} lineHeight="copy">
         {children}
       </Text>
