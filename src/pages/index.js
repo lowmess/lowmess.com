741c31fcd859a6fef7622712456f2f1407a5b64a
Alec Lomas <alec@lowmess.com>
2018-02-02 15:16:21 -0700

    :speech_balloon: add Fuel Made link

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,23 +11,45 @@ const indexPage = ({ data }) => {
   return (
     <article>
       <header>
-        <Title fontSize={[4, 5]} fontWeight="7" lineHeight="title" mt={0} mb={3}>
+        <Title
+          fontSize={[4, 5]}
+          fontWeight="7"
+          lineHeight="title"
+          mt={0}
+          mb={3}
+        >
           Hi! I&rsquo;m Alec&nbsp;Lomas.
         </Title>
         <Subtitle fontSize={[3, 4]} fontWeight="5" lineHeight="title" my={3}>
-          I&rsquo;m a frontend developer &amp; designer in Tempe,&nbsp;AZ.
+          I&rsquo;m a frontend developer &amp; designer at{' '}
+          <a href="https://fuelmade.com">
+            <Text color="black" hover={{ color: 'orange' }}>
+              Fuel&nbsp;Made
+            </Text>.
+          </a>
         </Subtitle>
         <Paragraph fontSize={[2, 3]} lineHeight="copy" mt={3} mb={4}>
-          My goal is to combine the principles of classic graphic design with the flexible and forward-thinking tenets
-          of the internet. I care deeply about legibility, performance, and the open web. And&nbsp;burritos.
+          My goal is to combine the principles of classic graphic design with
+          the flexible and forward-thinking tenets of the internet. I care
+          deeply about legibility, performance, and the open web.
+          And&nbsp;burritos.
         </Paragraph>
         <Rule mt={4} mb={5} />
       </header>
       <main>
-        <SectionTitle fontSize={[3, 4]} fontWeight="7" lineHeight="title" mt={5} mb={4}>
+        <SectionTitle
+          fontSize={[3, 4]}
+          fontWeight="7"
+          lineHeight="title"
+          mt={5}
+          mb={4}
+        >
           Latest Project
         </SectionTitle>
-        <ProjectPreview project={data.allProjectsJson.edges[0].node} level="h4" />
+        <ProjectPreview
+          project={data.allProjectsJson.edges[0].node}
+          level="h4"
+        />
       </main>
     </article>
   )
