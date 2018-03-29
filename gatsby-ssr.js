3540d12d1ec4d3929538905c34cd9c7ef048bfd7
Alec Lomas <alec@lowmess.com>
2018-03-29 00:19:24 -0700

    :sparkles: help inquiring minds fond the colophon

---
+++ gatsby-ssr.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  const logger =
+    process.env.NODE_ENV === 'production' ? (
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html:
+            "console.log('Wondering how this site was made? Check out the colophon. https://lowmess.com/colophon/')",
+        }}
+      />
+    ) : (
+      ''
+    )
+
+  setPostBodyComponents([logger])
+}
