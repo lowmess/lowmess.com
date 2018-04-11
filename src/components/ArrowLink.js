1235c19377ab4fcaed529dbbb04a80f8b88393ab
Alec Lomas <alec@lowmess.com>
2018-04-10 19:34:58 -0700

    :bug: center arrow link icon

--- src/components/ArrowLink.js
+++ src/components/ArrowLink.js
@@ -22,6 +22,7 @@ const ArrowLink = ({ dest, text, external, ...props }) => {
       <Text
         fontFamily="monospace"
         display="inline-flex"
+        align="center"
         color="black"
         hover={{ color: 'orange' }}
         fontSize={[0, 1]}
