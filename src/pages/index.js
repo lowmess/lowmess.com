ca4b1efea82018ecdab408b387ed80bf487bf620
Alec Lomas <alec@lowmess.com>
2019-02-20 14:42:23 -0700

    :art: remove unneeded fragment from homepage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,46 +26,44 @@ const IndexPage = () => {
   `)
 
   return (
-    <>
-      <article>
-        <Header>
-          <Title>Hi! I&rsquo;m Alec&nbsp;Lomas.</Title>
+    <article>
+      <Header>
+        <Title>Hi! I&rsquo;m Alec&nbsp;Lomas.</Title>
 
-          <Subtitle my={3}>
-            I&rsquo;m a frontend developer &amp; designer{' '}
-            <HeaderLink href="mailto:alec@lowmess.com?subject=Let's%20Work%20Together">
-              looking for new&nbsp;opportunities
-            </HeaderLink>
-            .
-          </Subtitle>
+        <Subtitle my={3}>
+          I&rsquo;m a frontend developer &amp; designer{' '}
+          <HeaderLink href="mailto:alec@lowmess.com?subject=Let's%20Work%20Together">
+            looking for new&nbsp;opportunities
+          </HeaderLink>
+          .
+        </Subtitle>
 
-          <Paragraph fontSize={[2, 3]} mt={3} mb={4}>
-            My goal is to combine the principles of classic graphic design with
-            the flexible and forward-thinking tenets of the internet. I care
-            deeply about legibility, performance, and the open web.
-            And&nbsp;burritos.
-          </Paragraph>
-        </Header>
+        <Paragraph fontSize={[2, 3]} mt={3} mb={4}>
+          My goal is to combine the principles of classic graphic design with
+          the flexible and forward-thinking tenets of the internet. I care
+          deeply about legibility, performance, and the open web.
+          And&nbsp;burritos.
+        </Paragraph>
+      </Header>
 
-        <main>
-          <Text
-            is="h2"
-            fontSize={[3, 4]}
-            fontWeight="7"
-            lineHeight="title"
-            mt={5}
-            mb={4}
-          >
-            Latest Project
-          </Text>
+      <main>
+        <Text
+          is="h2"
+          fontSize={[3, 4]}
+          fontWeight="7"
+          lineHeight="title"
+          mt={5}
+          mb={4}
+        >
+          Latest Project
+        </Text>
 
-          <ProjectPreview
-            project={data.allProjectsJson.edges[0].node}
-            level="h3"
-          />
-        </main>
-      </article>
-    </>
+        <ProjectPreview
+          project={data.allProjectsJson.edges[0].node}
+          level="h3"
+        />
+      </main>
+    </article>
   )
 }
 
