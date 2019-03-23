d7b12aa897bfa899e5783d51345aa0be948e1c48
Alec Lomas <alec@lowmess.com>
2019-03-22 23:49:01 -0700

    :speech_balloon: prevent widows on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -28,7 +28,7 @@ const AboutPage = () => {
             &ldquo;I&rsquo;m a frontend developer with a passion for legibility,
             performance, and the open web. A quick learner with a thirst for
             knowledge. Enthusiastic and hard-working, I pride myself on my
-            attention to detail.&rdquo;
+            attention to&nbsp;detail.&rdquo;
           </Paragraph>
 
           <Paragraph fontSize={[1, 2]} lineHeight="copy" mt={3} mb={3}>
@@ -37,8 +37,8 @@ const AboutPage = () => {
               my r&eacute;sum&eacute;
             </AboutLink>{' '}
             says. Kind of boring, huh? Well, it&rsquo;s a r&eacute;sum&eacute;.
-            They&rsquo;re supposed to be boring. (Don&rsquo;t be such a
-            nitpicker.)
+            They&rsquo;re supposed to be boring. (Don&rsquo;t be such
+            a&nbsp;nitpicker.)
           </Paragraph>
 
           <Paragraph fontSize={[1, 2]} lineHeight="copy" mt={3} mb={0}>
@@ -46,7 +46,8 @@ const AboutPage = () => {
             (don&rsquo;t worry about it), my favorite beer is all of them, and I
             like to list things in threes. If you have any questions, I&rsquo;m
             happy to send you a non sequitur and/or fully-serious response if
-            you <AboutLink href="mailto:alec@lowmess.com">email me</AboutLink>.
+            you{' '}
+            <AboutLink href="mailto:alec@lowmess.com">email&nbsp;me</AboutLink>.
           </Paragraph>
 
           <Stats mt={[4, 5]} />
