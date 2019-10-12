4f40c6cc810ff18a2b75358d1f9fc84ba3c50c15
Alec Lomas <alec@lowmess.com>
2019-10-12 01:02:26 -0700

    :sparkles: replace autolink-headers with custom component

--- src/components/MarkdownContent/MarkdownContent.js
+++ src/components/MarkdownContent/MarkdownContent.js
@@ -2,9 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { MDXProvider } from '@mdx-js/react'
 import { Box } from 'rebass'
+import * as Heading from './Headings'
+
+const components = {
+  h1: Heading.One,
+  h2: Heading.Two,
+  h3: Heading.Three,
+  h4: Heading.Four,
+  h5: Heading.Five,
+  h6: Heading.Six,
+}
 
 const MarkdownContent = ({ children, ...props }) => (
-  <MDXProvider>
+  <MDXProvider components={components}>
     <Box
       sx={{
         // Vertical rhythm
