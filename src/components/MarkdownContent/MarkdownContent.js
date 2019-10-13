ed93320a02bea4da9c8e1846bbc4a91b1e2ad428
Alec Lomas <alec@lowmess.com>
2019-10-12 22:06:55 -0700

    :lipstick: re-style details components
    was making the details post looks like shit

--- src/components/MarkdownContent/MarkdownContent.js
+++ src/components/MarkdownContent/MarkdownContent.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { MDXProvider } from '@mdx-js/react'
 import { Box } from 'rebass'
 import components from './components'
-import { verticalRhythm } from './markdownStyles'
+import { verticalRhythm, blockShape } from './markdownStyles'
 
 const MarkdownContent = ({ children, ...props }) => (
   <MDXProvider components={components}>
@@ -29,6 +29,12 @@ const MarkdownContent = ({ children, ...props }) => (
           marginY: 4,
           border: 1,
           borderColor: 'grays.2',
+        },
+
+        details: {
+          ...blockShape,
+          marginY: 4,
+          backgroundColor: 'grays.1',
         },
       }}
       {...props}
