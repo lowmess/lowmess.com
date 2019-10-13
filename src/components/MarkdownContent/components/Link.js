1416da18f7c139098445966198ab4cf35dc2b1fe
Alec Lomas <alec@lowmess.com>
2019-10-12 21:48:04 -0700

    :lipstick: update hover style of linked inline code

--- src/components/MarkdownContent/components/Link.js
+++ src/components/MarkdownContent/components/Link.js
@@ -2,7 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link as L } from 'rebass'
 
-const Link = ({ children, ...props }) => <L {...props}>{children}</L>
+const Link = ({ children, ...props }) => (
+  <L
+    sx={{
+      '&:hover code': {
+        color: 'orange',
+      },
+    }}
+    {...props}
+  >
+    {children}
+  </L>
+)
 
 Link.propTypes = {
   children: PropTypes.node.isRequired,
