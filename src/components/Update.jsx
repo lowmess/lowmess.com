34943e252cc5adf48e41ab5efb830d49fd7dbb35
Alec Lomas <alec@lowmess.com>
2020-04-25 17:17:21 -0700

    :recycle: make Update text a heading

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, Text, Container } from 'theme-ui'
+import { Box, Heading, Container } from 'theme-ui'
 
 const Update = ({ date, sx, children, ...props }) => (
   <Box bg="muted" my={5} py={4}>
     <Container sx={{ maxWidth: 'mdx-measure', ...sx }} {...props}>
-      {date && (
-        <Text variant="section-heading" mb={3}>
-          Update ({date})
-        </Text>
-      )}
+      <Heading as="h2" variant="section-heading" mb={3}>
+        Update {date && <span>({date})</span>}
+      </Heading>
 
       <Box as="p" sx={{ fontSize: [3, null, 4] }}>
         {children}
