c75f53b2536f0a47c76cf820a4373497ab95379e
Alec Lomas <alec@lowmess.com>
2020-05-30 15:13:31 -0700

    :bug: actually import MDX link component

--- src/gatsby-plugin-theme-ui/components.ts
+++ src/gatsby-plugin-theme-ui/components.ts
@@ -1,5 +1,6 @@
 import { h1, h2, h3, h4, h5, h6 } from './components/Headings'
 import p from './components/Paragraph'
+import a from './components/Link'
 import { UnorderedList as ul, OrderedList as ol } from './components/Lists'
 import { Code as code, InlineCode as inlineCode } from './components/Code'
 import blockquote from './components/BlockQuote'
@@ -13,6 +14,7 @@ export default {
   h5,
   h6,
   p,
+  a,
   ul,
   ol,
   code,
