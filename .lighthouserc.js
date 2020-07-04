6a6017ee2c0982fe00f89f391c162753bfa882db
Alec Lomas <alec@lowmess.com>
2020-07-03 19:16:32 -0700

    :construction_worker: ignore lighthouse errors thrown by 404 page
    kinda clumsily, but still

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,8 +3,12 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
+        // I don't have control over these (Gatsby does):
         'uses-rel-preconnect': 'off',
         'unused-javascript': 'off',
+        // 404 page (and only 404 page) causes these to error:
+        'tap-targets': 'warn',
+        'errors-in-console': 'warn',
         // Superflous text like "Introduction" on the homepage has a really
         // low color contrast, but is also not essential text. I do still
         // want warnings for other elements, though.
