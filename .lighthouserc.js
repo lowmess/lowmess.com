67d35f316ed44189e24b6b2ff3e0555ccbc80175
Alec Lomas <alec@lowmess.com>
2020-07-03 19:02:51 -0700

    :wrench: update lighthouse assertions
    some things i don't have control over

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,8 +3,11 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
+        'uses-rel-preconnect': 'off',
+        'unused-javascript': 'off',
         // Superflous text like "Introduction" on the homepage has a really
-        // low color contrast, but is also not essential text.
+        // low color contrast, but is also not essential text. I do still
+        // want warnings for other elements, though.
         'color-contrast': 'warn',
       },
     },
