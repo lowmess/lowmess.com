9e1efd66ed2a2c44a24fbc4d53b1379a72ad21a2
Alec Lomas <alec@lowmess.com>
2020-10-08 23:57:13 -0700

    :wheelchair: add lang attribute to html element

---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import * as React from 'react'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
