e3d34b0c2e97a7b1cfc4c698757fb94100872da6
Alec Lomas <alec@lowmess.com>
2020-10-15 19:22:32 -0700

    :speech_balloon: add years to archive page

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -5,6 +5,10 @@ import Stack from '../components/Stack'
 import { Header, HeaderName, HeaderTitle } from '../components/Header'
 import metadata from '../constants/metadata.json'
 
+const Dates: React.FC = (props) => (
+  <Text as="small" sx={{ fontSize: 1 }} {...props} />
+)
+
 const Description: React.FC = (props) => (
   <Text as="p" sx={{ maxWidth: 'measure' }} {...props} />
 )
@@ -29,13 +33,32 @@ const Archive: React.FC = () => (
       <Stack gap={5}>
         <Stack gap={3}>
           <Heading>
-            <Link href="https://v1.lowmess.com">Version 1</Link>
+            Version 0 <Dates>(2014 &ndash; 2015)</Dates>
           </Heading>
 
           <Description>
-            My very first portfolio site. Mostly it was a showcase for the
-            design work I did as a student, though I did also write the code
-            myself (I try not to think about the quality of the&nbsp;code).
+            The code for this version is (perhaps fortunately) lost to time. The
+            only thing I remember about it is that it had similar navigation to
+            v1. And that it was built as a student project using student design
+            work. That&rsquo;s called&nbsp;efficiency.
+          </Description>
+
+          <Tooling>
+            Built with <Link href="https://jquery.com">jQuery</Link>, I assume.
+          </Tooling>
+        </Stack>
+
+        <Stack gap={3}>
+          <Heading>
+            <Link href="https://v1.lowmess.com">Version 1</Link>{' '}
+            <Dates>(2015 &ndash; 2017)</Dates>
+          </Heading>
+
+          <Description>
+            Mostly this was a showcase for the design work I did as a student,
+            but if you look closely you can find some client work. The copy (and
+            color contrast) on this site is really bad, and I find it&rsquo;s
+            best to not think about the quality of the&nbsp;code.
           </Description>
 
           <Tooling>
@@ -48,14 +71,14 @@ const Archive: React.FC = () => (
 
         <Stack gap={3}>
           <Heading>
-            <Link href="https://v2.lowmess.com">Version 2</Link>
+            <Link href="https://v2.lowmess.com">Version 2</Link>{' '}
+            <Dates>(2017)</Dates>
           </Heading>
 
           <Description>
-            The first portfolio I created for myself after focusing on
-            development. I hated the design on this one basically as soon as I
-            launched it, and I think it only lasted around 3 months before I
-            scrapped it for&nbsp;v3.
+            The first portfolio I made as a developer. I hated the design on
+            this one basically as soon as I launched it, and it lasted fewer
+            than 6 months before I scrapped it for&nbsp;v3.
           </Description>
 
           <Tooling>
@@ -69,7 +92,8 @@ const Archive: React.FC = () => (
 
         <Stack gap={3}>
           <Heading>
-            <Link href="https://v3.lowmess.com">Version 3</Link>
+            <Link href="https://v3.lowmess.com">Version 3</Link>{' '}
+            <Dates>(2017 &ndash; 2020)</Dates>
           </Heading>
 
           <Description>
@@ -89,7 +113,8 @@ const Archive: React.FC = () => (
 
         <Stack gap={3}>
           <Heading>
-            <Link href="https://v4.lowmess.com">Version 4</Link>
+            <Link href="https://v4.lowmess.com">Version 4</Link>{' '}
+            <Dates>(2020 &ndash; present)</Dates>
           </Heading>
 
           <Description>
