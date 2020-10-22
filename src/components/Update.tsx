755b2e1f5bc70be6ae0eb30e59013b758db0138e
Alec Lomas <alec@lowmess.com>
2020-10-21 21:35:45 -0700

    :lipstick: make inline updates more streamlined

--- src/components/Update.tsx
+++ src/components/Update.tsx
@@ -4,16 +4,30 @@ import { ThemeUIProps } from '../types/ThemeUIComponent'
 
 interface Props extends ThemeUIProps {
   date?: string
+  inline?: boolean
 }
 
-const Update: React.FC<Props> = ({ date, sx, children, ...props }) => (
-  <Box bg="muted" my={5} py={4}>
+const Update: React.FC<Props> = ({
+  date,
+  inline = false,
+  sx,
+  children,
+  ...props
+}) => (
+  <Box
+    bg="muted"
+    sx={{
+      marginY: inline ? null : 5,
+      paddingY: 4,
+    }}
+  >
     <Container sx={{ maxWidth: 'mdx-measure', ...sx }} {...props}>
       <Heading as="h2" variant="section-heading" mb={3}>
         Update {date && <span>({date})</span>}
       </Heading>
 
-      <Box as="p" sx={{ fontSize: [3, null, 4] }}>
+      {/* inline font-size taken from `Paragraph.tsx` */}
+      <Box as="p" sx={{ fontSize: inline ? [null, null, 3] : [3, null, 4] }}>
         {children}
       </Box>
     </Container>
