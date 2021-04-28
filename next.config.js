8b159601bb6933c38c98f4b074965bf9a11af7d1
Alec Lomas <alec@lowmess.com>
2021-04-27 19:12:21 -0700

    :lock: add headers

--- next.config.js
+++ next.config.js
@@ -35,8 +35,64 @@ const withMDX = require('next-mdx-enhanced')({
 	},
 })
 
+const contentSecurityPolicy = `
+	default-src 'self';
+	script-src 'self' 'unsafe-eval' 'unsafe-inline' *.codepen.io;
+	child-src *.codepen.io;
+	style-src 'self' 'unsafe-inline';
+	img-src * blob: data:;
+	media-src 'none';
+	connect-src *;
+	font-src 'self';
+`
+
+const securityHeaders = [
+	{
+		key: 'Content-Security-Policy',
+		value: contentSecurityPolicy.replace(/\n/g, ''),
+	},
+	{
+		key: 'Referrer-Policy',
+		value: 'origin-when-cross-origin',
+	},
+	{
+		key: 'X-Frame-Options',
+		value: 'DENY',
+	},
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	},
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=31536000; includeSubDomains; preload',
+	},
+	{
+		key: 'Permissions-Policy',
+		value: 'camera=(), microphone=(), geolocation=()',
+	},
+]
+
 const nextConfig = {
 	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+
+	// eslint-disable-next-line require-await
+	async headers() {
+		return [
+			{
+				source: '/',
+				headers: securityHeaders,
+			},
+			{
+				source: '/:path*',
+				headers: securityHeaders,
+			},
+		]
+	},
 }
 
 module.exports = withTM(withMDX(nextConfig))
