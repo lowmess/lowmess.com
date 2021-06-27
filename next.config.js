0ebc2004b6494744f43e7f6c1b02c569a8088a75
Alec Lomas <alec@lowmess.com>
2021-06-26 23:57:37 -0700

    :wrench: use next-mdx

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { format, addMinutes } = require('date-fns')
-
 const withTM = require('next-transpile-modules')([
 	'react-children-utilities',
 	'lowmess-prism',
@@ -7,31 +5,10 @@ const withTM = require('next-transpile-modules')([
 
 const smartypants = require('@silvenon/remark-smartypants')
 
-const withMDX = require('next-mdx-enhanced')({
-	layoutPath: 'src/layouts',
-	defaultLayout: true,
-	usesSrc: true,
-	remarkPlugins: [smartypants],
-	extendFrontMatter: {
-		process: (_, frontMatter) => {
-			const { date, __resourcePath } = frontMatter
-
-			const datetime = date.toISOString()
-			const year = date.getFullYear()
-			const offset = date.getTimezoneOffset()
-
-			const url = `/${__resourcePath.replace(/\.mdx?$/, '')}`
-
-			const slug = url.split('/').pop()
-
-			return {
-				datetime,
-				date: format(addMinutes(frontMatter.date, offset), 'MMMM d, yyyy'),
-				year,
-				url,
-				slug,
-			}
-		},
+const withMDX = require('@next/mdx')({
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [smartypants],
 	},
 })
 
