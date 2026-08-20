e260d0fd1665405067454c9a381efdfded2c2127
Alec Lomas <alec@lowmess.com>
2026-08-20 01:29:18 -0700

    :lipstick: tweak colors; set typefaces; design footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,27 @@
 import sitemap from "@astrojs/sitemap";
 import { transformerNotationDiff } from "@shikijs/transformers";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField, fontProviders } from "astro/config";
 
 import { alabasterTheme, rubberTheme } from "./syntax-themes";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://lowmess.com",
+	redirects: {
+		"/projects": "/work",
+	},
+	integrations: [
+		sitemap({ filter: (page) => !page.includes("/blog/archive") }),
+	],
+	env: {
+		schema: {
+			LASTFM_API_KEY: envField.string({ context: "client", access: "public" }),
+			LASTFM_USER_NAME: envField.string({
+				context: "client",
+				access: "public",
+			}),
+		},
+	},
 	markdown: {
 		shikiConfig: {
 			defaultColor: false,
@@ -17,10 +32,44 @@ export default defineConfig({
 			transformers: [transformerNotationDiff()],
 		},
 	},
-	integrations: [
-		sitemap({ filter: (page) => !page.includes("/blog/archive") }),
+	fonts: [
+		{
+			name: "Framboisier",
+			cssVariable: "--typeface-framboisier",
+			provider: fontProviders.local(),
+			options: {
+				variants: [
+					{
+						src: ["./src/assets/fonts/framboisier-bold.woff"],
+						weight: "400 700",
+						style: "normal",
+					},
+					{
+						src: ["./src/assets/fonts/framboisier-bolditalic.woff"],
+						weight: "400 700",
+						style: "italic",
+					},
+				],
+			},
+			fallbacks: ["Georgia", "ui-serif", "serif"],
+		},
+		{
+			name: "Hanken Grotesk",
+			cssVariable: "--typeface-hanken-grotesk",
+			provider: fontProviders.fontsource(),
+			styles: ["normal", "italic"],
+			weights: ["400 700"],
+			subsets: ["latin"],
+			fallbacks: ["ui-sans-serif", "system-ui", "sans-serif"],
+		},
+		{
+			name: "Fira Code",
+			cssVariable: "--typeface-fira-code",
+			provider: fontProviders.fontsource(),
+			styles: ["normal"],
+			weights: ["400 700"],
+			subsets: ["latin"],
+			fallbacks: ["Menlo", "ui-monospace", "monospace"],
+		},
 	],
-	redirects: {
-		"/projects": "/work",
-	},
 });
